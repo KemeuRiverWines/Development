dbb7747cbca9681c6c530a69190e522811062151
SirAlpha <102395260+SirAlphaa@users.noreply.github.com>
2023-07-18 13:44:22 +1200

    Bring in Gavins Stuff

---
+++ GavinStuff/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import 'react-native-gesture-handler';
+import TemperatureScreen from './TemperatureScreen';
+import WeatherPage from './WeatherPage';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name="TemperatureScreen" component={TemperatureScreen} />
+      <Stack.Screen name="WeatherPage" component={WeatherPage} />
+    </Stack.Navigator>
+  </NavigationContainer>
+  );
+};
+
+export default App;
