3c9cfc2dbdd2d5432ae16e6cdd060926bb754e71
SirAlpha <102395260+SirAlphaa@users.noreply.github.com>
2023-07-20 14:15:38 +1200

    Updated Function_KemeuRiverWines with a new Components file and edited the weaperAirTemp.js file

--- Software Team/Function_KemeuRiverWines/TemperatureScreen.js
+++ Software Team/Function_KemeuRiverWines/TemperatureScreen.js
@@ -7,46 +7,9 @@ import { useNavigation } from '@react-navigation/native';
 import axios from 'axios';
 import { encode } from 'base-64';
 
+import WeatherAirTemp from './Components/WeatherAirTemp';
 
 const TemperatureScreen = () => {
-  const [temperature, setTemperature] = useState(null);
-
-  useEffect(() => {
-      const fetchData = async () => {
-          const currentDate = new Date();
-
-          const year = currentDate.getFullYear();
-          const month = (currentDate.getMonth() + 1).toString().padStart(2, '0');
-          const day = currentDate.getDate().toString().padStart(2, '0');
-          const hours = currentDate.getHours().toString().padStart(2, '0');
-          const minutes = (currentDate.getMinutes()).toString().padStart(2, '0');
-          const formattedDate = `${year}-${month}-${day}`;
-          const formattedTime = `${hours}:${minutes}:00.000`;
-
-          const url = `https://api.meteomatics.com/${formattedDate}T${formattedTime}+12:00/t_2m:C/-36.852095,174.7631803/json?model=mix`;
-          const username = 'aucklanduniversityoftechnology_ali';
-          const password = 'i4b8tLSu6A';
-
-          
-          try {
-              const encodedCredentials = encode(`${username}:${password}`);
-              const response = await axios.get(url, {
-                  headers: {
-                      Authorization: `Basic ${encodedCredentials}`,
-                  },
-              });
-              
-              const temperature = response.data.data[0].coordinates[0].dates[0].value;
-              setTemperature(temperature);
-              
-              console.log(`Weather Air Temp Successful (Meteomatics Weather API) = ${url}`);
-          } catch (error) {
-              console.error('API Error:', error);
-          }
-      };
-
-      fetchData();
-  }, []);
 
   const navigation = useNavigation();
   const categoryOptions = ['Nodes', 'Functions', 'Map'];
@@ -96,11 +59,11 @@ const TemperatureScreen = () => {
   return (
     <View style={styles.container}>
       <View style={styles.topContainer}>
-        <Text style={styles.headerText}>Kumeu River Vineyard Temp</Text>
+        <Text style={styles.headerText}>Kumeu River Vineyard Temperature</Text>
         <View style={styles.temperatureContainer}>
           <View style={styles.temperatureBackground}>
             <Text style={styles.temperature}>
-              {temperature !== null ? `${temperature}°C` : 'Loading...'}
+              <WeatherAirTemp />
             </Text>
           </View>
         </View>
@@ -192,7 +155,7 @@ const styles = StyleSheet.create({
     paddingVertical: 48,
   },
   headerText: {
-    fontSize: 18,
+    fontSize: 25,
     fontWeight: 'bold',
     color: '#FFFFFF',
     marginBottom: 8,
