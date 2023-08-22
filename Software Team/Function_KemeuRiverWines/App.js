20fbcd69c7366dc4cb2dafed89719a7543b613ab
SirAlphaa <captainscar117@gmail.com>
2023-08-22 19:29:32 +1200

    Bug Fixing

--- Software Team/Function_KemeuRiverWines/App.js
+++ Software Team/Function_KemeuRiverWines/App.js
@@ -4,13 +4,16 @@ import { StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
+
 import TemperatureScreen from './screens/TemperatureScreen';
 import WeatherPage from './screens/WeatherPage';
 import TestComponent from './screens/TestComponent';
 
 const Stack = createStackNavigator();
 
+
 const App = () => {
+
   return (
     <>
       <StatusBar barStyle="light-content" />
