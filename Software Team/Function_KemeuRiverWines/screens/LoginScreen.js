90a89c2c8e037dcc5916f08746f9c0fa0fe02c13
SirAlpha <102395260+SirAlphaa@users.noreply.github.com>
2023-10-17 19:03:57 +1300

    Removed Forgot Pin Button

--- Software Team/Function_KemeuRiverWines/screens/LoginScreen.js
+++ Software Team/Function_KemeuRiverWines/screens/LoginScreen.js
@@ -68,7 +68,7 @@ const PinScreen = () => {
         ))}
       </View>
       <View style={styles.numberPad}>
-        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 'Forget PIN?', 0, 'delete'].map((num) => (
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, '', 0, 'delete'].map((num) => (
           <TouchableOpacity
             key={num.toString()}
             style={styles.numberButton}
